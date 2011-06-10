17431e5e6dc620c0e19c6e15a8885b7914b28719
Kristján Pétursson <kristjan@causes.com>
2011-06-09 18:54:37 -0700

    Only consider unit-based tiles for now

--- lib/mosaic.rb
+++ lib/mosaic.rb
@@ -1,20 +1,14 @@
 class Mosaic
   attr_reader   :rows, :cols
-  attr_accessor :width, :height
 
-  def initialize(rows, cols, width=cols, height=rows)
-    @rows, @cols, @width, @height = rows, cols, width, height
-    validate
+  attr_reader :tiles
+
+  def initialize(rows, cols)
+    @rows, @cols = rows, cols
+    generate!
   end
 
-  private
-
-  def validate
-    unless @height % @rows == 0
-      raise ArgumentError.new("Height should be a multiple of rows")
-    end
-    unless @width % @cols == 0
-      raise ArgumentError.new("Width should be a multiple of cols")
-    end
+  def generate!
+    @grid = Array.new(@rows) {Array.new(@cols)}
   end
 end
